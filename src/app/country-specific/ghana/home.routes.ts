6a6efc70d1da562fd63e618b6feaaa76a8f91732
Samuel Acquah <samuelacquah@Lenovo-YOGA-730-15IKB-7544041e.localdomain>
2025-04-27 06:56:19 +0000

    feat: add cart route and improve product card layout

--- src/app/country-specific/ghana/home.routes.ts
+++ src/app/country-specific/ghana/home.routes.ts
@@ -1,9 +1,14 @@
 import { RouterModule, Routes } from "@angular/router";
 import { IndexComponent } from "./pages/index/index.component";
+import { CartComponent } from "./pages/cart/cart.component";
 
 export const HomepageInRoutes: Routes = [
    {
       path: "",
       component : IndexComponent
+   },
+   {
+      path: "cart",
+      component : CartComponent
    }
 ]
